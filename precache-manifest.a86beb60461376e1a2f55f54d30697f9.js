4179393375d267e9dfa2498d7da4b93514ed943f
Prajwal Shah <prajwalshah1721@gmail.com>
2020-07-02 23:30:12 +0530

    Updates

---
+++ precache-manifest.a86beb60461376e1a2f55f54d30697f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "468866b67e5d86d3246600f95b5d23a3",
+    "url": "/monster_app/index.html"
+  },
+  {
+    "revision": "84ba8330bc349aa865d3",
+    "url": "/monster_app/static/css/main.ffc3ab48.chunk.css"
+  },
+  {
+    "revision": "299f65059b0504546074",
+    "url": "/monster_app/static/js/2.eca8dab0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster_app/static/js/2.eca8dab0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84ba8330bc349aa865d3",
+    "url": "/monster_app/static/js/main.0fb467cc.chunk.js"
+  },
+  {
+    "revision": "9870b3de0c435924d0e0",
+    "url": "/monster_app/static/js/runtime-main.c8baec6c.js"
+  }
+]);
